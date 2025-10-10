f41f9bf076ee34bbfd58adb52e4e9f3f09e014ed
Paulo Victor Rezende <pvrezende2023@gmail.com>
2025-10-09 20:25:34 -0400

    Versão final com todas as correções

--- frontend/config.js
+++ frontend/config.js
@@ -2,8 +2,11 @@
 
 // Daqui para frente, altere APENAS este IP quando mudar de rede
 // frontend/config.js
-const API_IP = '100.26.141.230'; 
-export const API_URL = `http://${API_IP}:3001`;
+//const API_IP = '100.26.141.230'; 
+//export const API_URL = `http://${API_IP}:3001`;
+
+// A URL agora é fixa e aponta para o servidor na nuvem (Render).
+export const API_URL = 'https://controle-gastos-api-paulo.onrender.com';
 
 
 //const API_URL = 'http://192.168.1.186:3000';//delta
