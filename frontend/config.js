378f4a703d80284fb14db5ea01e2a293ec79f229
Paulo Rezende <paulo.rezende@deltasollutions.com.br>
2025-10-26 18:46:14 -0400

    Atualiza API_URL para HTTPS e domínio ddns.net

--- frontend/config.js
+++ frontend/config.js
@@ -1,12 +1,17 @@
 // frontend/config.js
 
+// Novo domínio configurado com HTTPS via Nginx e Certbot
+const API_DOMAIN = 'app-gastos.ddns.net';
+// Use a variável API_DOMAIN para construir a URL completa com https://
+export const API_URL = `https://${API_DOMAIN}`;
+
+
 // Use o DNS Público ou o IP Público da sua instância EC2
 // O DNS é geralmente preferível pois o IP público pode mudar se a instância for parada/iniciada.
-const API_IP = 'ec2-100-26-141-230.compute-1.amazonaws.com'; // DNS Público da sua EC2
-export const API_URL = `http://${API_IP}:3001`; // Porta definida no backend (server.js)
-
+//const API_IP = 'ec2-100-26-141-230.compute-1.amazonaws.com';
+//export const API_URL = `//http://${API_IP}:3001`;
 // IPs antigos comentados (para referência)
-//const API_URL = 'http://192.168.0.187:3000';//delta
+//const API_URL = 'http://199.192.191.139:3000';//delta
 //const API_URL = 'http://192.168.1.85:3000';//casa
 //const API_URL = 'http://10.60.104.105:3000';//indt
 //const API_URL = 'http://10.176.56.192:3000';//celular
